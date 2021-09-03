dc51c8ff12d891ad99036404c729956ef8c936a9
Sebastian Georgi <sebastian@georgi.io>
2021-09-03 08:37:31 +0200

    Additonal route

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/secret")]
+fn secret() -> &'static str {
+    "Hello, secret world!"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, secret])
 }
